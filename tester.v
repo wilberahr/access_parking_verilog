95718f04c6f0853b12a32d20bf1216673b07a877
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-09-27 17:11:22 -0600

    Se agrega el bloque de initial para realizar las pruebas de funcionamiento. Se agregan las 4 pruebas en comentarios y se agregan los estimulos de las señales del modulo access del DUT.

--- tester.v
+++ tester.v
@@ -14,5 +14,79 @@ module tester(
      input wire senal_compuerta, senal_alarma_pin, senal_alarma_bloqueo;
 
      
+     initial begin
+     //Incializacion en bajo del clock y en alto del reset
+     // al inicio de los tiempos
+     clock = 0;
+     reset = 1;
+     //inicializacion en bajo de los sensores al inicio de la simulacion
+     sensor_llegada_vehiculo = 0, 
+     sensor_ingreso_vehiculo = 0,
+     
+     //Activación en bajo del reset
+     #5 reset = 0;
+     //Desactivación en alto del reset
+     #15 reset = 1;
+     
+     //////////////////////////////////////////
+     //Prueba 1: funcionamiento normal básico
+     //
+     // Llegada de un vehículo
+     #20 sensor_llegada_vehiculo = 1;
+     // Ingreso del pin correcto y apertura de puerta
+     #20 clave_ingresada = 4'h3257;
+     // Sensor de fin de entrada y cierre de compuerta.
+     #20 sensor_ingreso_vehiculo = 0;
+     
+
+
+     //Prueba 2: ingreso de pin incorrecto menos de 3 veces
+     //
+     // Llegada de un vehículo
+     #20 sensor_llegada_vehiculo = 1;
+     // Ingreso de pin incorrecto (una o dos veces), puerta permanece cerrada. 
+     #20 clave_ingresada = 4'h7523;
+     #20 clave_ingresada = 4'h4368;
+     // Ingreso de pin correcto, funcionamiento normal básico. 
+     #20 clave_ingresada = 4'h3257;
+     // Revisión de contador de intentos incorrectos.
+
+
+     //////////////////////////////////////////
+     //Prueba 3: Ingreso de pin incorrecto 3 o más veces
+     #20 clave_ingresada = 4'h7523;
+     #20 clave_ingresada = 4'h4368;
+     #20 clave_ingresada = 4'h2656;
+     // Revisión de alarma de pin incorrecto.
+     
+     // Revisión de contador de intentos incorrectos. 
+     
+     // Ingreso de pin correcto, funcionamiento normal básico. 
+     #20 clave_ingresada = 4'h3257;
+     // Revisión de limpieza de contadores y alarmas.
+
+     //////////////////////////////////////////
+     //Prueba 4: alarma de bloqueo
+     //
+     // Ambos sensores encienden al mismo tiempo, 
+     #20 sensor_llegada_vehiculo = 1;
+     #0 sensor_ingreso_vehiculo = 1;
+     // Encendido de alarma de bloqueo, 
+     
+     // Ingreso de clave incorrecta, bloqueo permanece. 
+     #20 clave_ingresada = 4'h5479;
+     // Ingreso de clave correcta, desbloqueo. 
+     #20 clave_ingresada = 4'h3257;
+     // Funcionamiento normal básico.
+
+     #200 $finish;
+     end
+
+     always begin
+     #5 clock = !clock;
+     end
+
+
+     
 
 endmodule
