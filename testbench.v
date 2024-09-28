dbeb71d4e19b7ece32f983ff240888d5c4e310ab
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-09-27 19:28:47 -0600

    Correcciones de sintaxis en código.

--- testbench.v
+++ testbench.v
@@ -1,23 +1,24 @@
 `include "tester.v"
-`include "dui.v"
+`include "dut.v"
 
-module testbench
+module testbench();
+
 
      wire clk, rst; 
      wire s_llegada_vehiculo, s_ingreso_vehiculo;
-     wire [15:0] clave_ingresada;
+     wire [15:0] clave;
      wire signal_compuerta, signal_alarma_pin, signal_alarma_bloqueo;
 
      initial begin
-          $dumpfile("resusultados.vcd")
+          $dumpfile("resultados.vcd");
           $dumpvars(-1,DUT);
-          $monitor("s_llegada_vehiculo = %b, s_ingreso_vehiculo = %b ,signal_compuerta = %b, signal_alarma_bloqueo = %b, signal_alarma_pin = %b"
-                    s_llegada_vehiculo,s_ingreso_vehiculo,signal_compuerta,signal_alarma_bloqueo,signal_alarma_pin)
+  //        $monitor("s_llegada_vehiculo = %b, s_ingreso_vehiculo = %b ,signal_compuerta = %b, signal_alarma_bloqueo = %b, signal_alarma_pin = %b"
+ //                   s_llegada_vehiculo,s_ingreso_vehiculo,signal_compuerta,signal_alarma_bloqueo,signal_alarma_pin);
 
      end
 
 
-     access DUT(
+     access  #(.CLAVE_CORRECTA(16'h3257)) DUT(
           .clock(clk), 
           .reset(rst), 
           .sensor_ingreso_vehiculo(s_ingreso_vehiculo),
@@ -37,4 +38,6 @@ module testbench
           .senal_compuerta(signal_compuerta),
           .senal_alarma_pin(signal_alarma_pin),
           .senal_alarma_bloqueo(signal_alarma_bloqueo)
-     ):
+     );
+
+endmodule
