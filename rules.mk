20ee612ec3a0f79f38962ad99eb19eaeadf58885
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-11-01 20:46:04 -0600

    Se agregaron expresiones regulares para la compilacion de archivos y simulación. Se agregó una reglas para crear directorios para guardar los archivos generados al correr el Makefile.

--- rules.mk
+++ rules.mk
@@ -1,14 +1,41 @@
-all: yosis gtkwave iverilog simulation
+
+all: yosis gtkwave iverilog simulation dir
+
+dir: $(SRC_DIR) $(LIB_DIR) $(BUILD_DIR) $(SIM_DIR) $(TESTS_DIR) $(SCRIPT_DIR)
+
+
+$(SRC_DIR):
+	mkdir -p $(SRC_DIR)
+	
+$(LIB_DIR):
+	mkdir -p $(LIB_DIR)
+	
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+	
+$(SIM_DIR):
+	mkdir -p $(SIM_DIR)
+
+$(TESTS_DIR):
+	mkdir -p $(TESTS_DIR)
+
+$(SCRIPT_DIR):
+	mkdir -p $(SCRIPT_DIR)
+
+#TODO agregar los .PHONY yosis gtkwave iverilog simulation dir
 
 yosis: $(SYNYH_FILE)
+	$(SYNYH_FILE)
 	
 gtkwave: $(TARGET)
 	gtkwave $(TARGET) 
 
 iverilog: $(OUTPUT)
 
-simulation: $(TARGET)
-
+yosis_simulation: yosis
+	iverilog -o $(OUTPUT)_synth $(TESTBENCH)
+	vpp $(OUTPUT)_synth
+	gtkwave $(TARGET)_synth.vcd
 
 
 $(TARGET): $(OUTPUT) 
@@ -21,23 +48,39 @@ $(OUTPUT): $(TESTBENCH)
 
 $(SYNYH_FILE): $(YS)
 	yosis -s $(YS)
-	iverilog -o $(OUTPUT)_synth $(TESTBENCH)
-	vpp $(OUTPUT)_synth
-	gtkwave $(TARGET)_synth.vcd
 
-$(YS): $(DUT_FILE) $(CMOS_LIB) $(DUT_FILE)
-	read_verilog $(DUT_FILE).v
-	hierarchy -check -top $(DUT)
-	proc
-	opt
-	fsm
-	opt
-	memory
-	opt
-	techmap
-	opt
-	dfflibmap -liberty ./$(CMOS_LIB)
-	abc -liberty ./cmos_cells.lib
-	show
-	clean
-	write_verilog $(SYNYH_FILE) 
+
+$(YS): $(DUT_FILE) $(CMOS_LIB) $(DUT).ys
+	echo "read_verilog $(DUT_FILE)" >>  $(DUT).ys
+	echo "hierarchy -check -top $(DUT)" >> $(DUT).ys
+	echo "proc; opt; fsm; opt; memory; opt" >> $(DUT).ys
+	echo "techmap; opt" >> $(DUT).ys
+	echo "dfflibmap -liberty ./$(CMOS_LIB)" >> $(DUT).ys
+	echo "abc -liberty ./cmos_cells.lib" >> $(DUT).ys
+	echo "show" >> $(DUT).ys
+	echo "clean" >> $(DUT).ys
+	echo "write_verilog $(SYNYH_FILE)" $(DUT).ys 
+
+#Expresiones regulares
+
+%.out: %.v
+	iverilog -o $@ $<
+
+%.vcd: %.out
+	vpp $<
+	gtkwave $@
+
+%.ys:
+	touch -p $@
+
+#TODO: regla clean y clear
+#TODO agregar los .PHONY
+
+clean:
+	rm salida
+	rm salidaYosis
+
+clear:
+	rm salida
+	rm salidaYosis
+	rm resultados.vcd
