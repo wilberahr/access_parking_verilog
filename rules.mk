31fd133e98407fb5142949380f165e3944372a33
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-11-01 19:07:17 -0600

    Se agrega codigo para compilar archivos de la sintesis con Yosis para hacer la simulacion.

--- rules.mk
+++ rules.mk
@@ -21,6 +21,9 @@ $(OUTPUT): $(TESTBENCH)
 
 $(SYNYH_FILE): $(YS)
 	yosis -s $(YS)
+	iverilog -o $(OUTPUT)_synth $(TESTBENCH)
+	vpp $(OUTPUT)_synth
+	gtkwave $(TARGET)_synth.vcd
 
 $(YS): $(DUT_FILE) $(CMOS_LIB) $(DUT_FILE)
 	read_verilog $(DUT_FILE).v
